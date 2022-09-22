5e924d9752efec8ba8c3f48e80819effbf92995f
Franco Du Plessis <francodup1005@gmail.com>
2022-09-22 15:25:12 +0200

    Generic repository creation

---
+++ ConnectedOffice/DeviceManagement_WebApp/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
--- ConnectedOffice/DeviceManagement_WebApp/Repositories/GenericRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repositories/GenericRepository.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp;
 
 namespace DeviceManagement_WebApp.Repositories
 {
@@ -17,6 +18,30 @@ namespace DeviceManagement_WebApp.Repositories
         public GenericRepository(ConnectedOfficeContext context)
         {
             _context = context;
+        }
+
+        //Adds an entity
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        //Gets all the records
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        //Gets the entity by ID
+        public T GetById (int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        //Removes the record from context
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
         }
     }
 }
