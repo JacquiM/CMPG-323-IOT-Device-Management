638a5a355e07a2025fe5901e711109f8b0d15432
MolefeSentshopac <molefet674@gmail.com>
2022-09-26 23:58:19 +0200

    Web App Development

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/IZoneRepository.cs
@@ -0,0 +1,11 @@
+﻿using DeviceManagement_WebApp.Models;
+using System;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface IZoneRepository : IGenericRepository<Zone>
+    {
+        bool exists(Guid id);
+        Zone GetMostRecentZone();
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,22 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository : GenericRepository<Zone>, IZoneRepository
+    {
+        public ZoneRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+        public bool exists(Guid id)
+        {
+            return _context.Zone.Any(e => e.ZoneId == id);
+        }
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(service => service.DateCreated).FirstOrDefault();
+        }
+    }
+}
