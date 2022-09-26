379f82efed22bcbc181302a0fe1e88e20beb0f9e
Student <Student@nwu.ac.za>
2022-09-26 12:56:12 +0200

    Need to fix a few errors

--- ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepo.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepo.cs
@@ -5,7 +5,19 @@ using System.Threading.Tasks;
 
 namespace DeviceManagement_WebApp.Repository
 {
-    public class CategoryRepo
+    public class CategoryRepo : Controllers
     {
+        private readonly ICategoryRepo categoryRepo;
+        public CategoriesController(ICategoryRepo categoryRepo)
+        {
+            _categoryRepository = categoryRepo;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(_categoryRepository.GetAll());
+        }
+
     }
 }
