20894068177bb8b62f47aff7dfe5ea34170da186
Hp Pc <Hp Pc@DESKTOP-0E5ER05>
2022-09-27 02:13:09 +0200

    Add ICategoryRepository

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+        Category GetMostRecentCategory();
+    }
+}
+
