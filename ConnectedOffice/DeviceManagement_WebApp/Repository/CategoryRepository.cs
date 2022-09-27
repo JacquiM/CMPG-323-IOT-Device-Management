b931d45309105994d565d863300a254f84c094c4
Hp Pc <Hp Pc@DESKTOP-0E5ER05>
2022-09-27 02:22:46 +0200

    Add CategoryRepository

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        //Get most recent Category taht was created and update it
+        public Category GetMostRecentCategory()
+        {
+            return _context.Category.OrderByDescending(Category => Category.DateCreated).FirstOrDefault();
+        }
+
+    }
+}
+
+
