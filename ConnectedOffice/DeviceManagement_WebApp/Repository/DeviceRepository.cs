f3f391922fae825f5e6fab32b32ea8c929b78b27
aucampdavid <davidaucamp8@gmail.com>
2022-09-27 20:58:35 +0200

    Devices Repo pattern and authentication

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/DeviceRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq.Expressions;
+using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Data;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class DeviceRepository : GenericRepository<Device>, IDeviceRepository
+    {
+        public DeviceRepository(ConnectedOfficeContext context) : base(context)
+        {
+        }
+
+        public Device GetMostRecentDevice()
+        {
+            return _context.Device.OrderByDescending(device => device.DateCreated).FirstOrDefault();
+        }
+    }
+
+}
+
