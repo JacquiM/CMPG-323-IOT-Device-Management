a7d9968d7bef2c74fcce47a2bd14fe35501e5722
Hp Pc <Hp Pc@DESKTOP-0E5ER05>
2022-09-27 04:50:30 +0200

    commit zone controller

--- ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Controllers/ZonesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DeviceManagement_WebApp.Data;
 using DeviceManagement_WebApp.Models;
+using DeviceManagement_WebApp.Repository;
 
 namespace DeviceManagement_WebApp.Controllers
 {
@@ -20,6 +21,7 @@ namespace DeviceManagement_WebApp.Controllers
             _context = context;
             _zoneRepository = zoneRepository;
         }
+        
 
         // GET: Zones
         public async Task<IActionResult> Index()
