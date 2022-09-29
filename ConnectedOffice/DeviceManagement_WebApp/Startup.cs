12b127a17dd4c180b79c4bf3a28cff7a8a0e0065
31884288 <31884288@V-CTRX-LAB-054>
2022-09-29 14:49:48 +0200

    push to github

--- ConnectedOffice/DeviceManagement_WebApp/Startup.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Startup.cs
@@ -1,4 +1,6 @@
 using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.RepoClasses;
+using DeviceManagement_WebApp.RepositoryClasses;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -37,6 +39,18 @@ namespace DeviceManagement_WebApp
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IDeviceRepository, DeviceRepository>();
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IZoneRepository, ZoneRepository>();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
